9191172d4a247ea3a1e3339c1cfa8e21007427bc
Archie <archieagarwal20012gmail.com>
2023-02-24 18:17:07 +0530

    changed

--- routes/downloadlink.js
+++ routes/downloadlink.js
@@ -1,6 +1,6 @@
 const route=require('express').Router();
 const File=require('../models/fileSchema');
-route.get('/:uuid', async (req, resp)=>{
+route.get(':uuid', async (req, resp)=>{
   const file=await File.findOne({uuid:req.params.uuid});
   if(!file){
     return resp.json({error: 'Link expired'});
