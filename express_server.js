d266d151414367eaecebc5fb53b317e7da76fe12
123archie <93096219+123archie@users.noreply.github.com>
2023-03-02 12:17:20 +0530

    changing

--- express_server.js
+++ express_server.js
@@ -1,19 +1,19 @@
 var express = require("express");
 // var cors=require("cors");
 // var bodyparser=require("body-parser");
-// const dotenv = require("dotenv");
-// const path = require("path");
+const dotenv = require("dotenv");
+const path = require("path");
 const app=express();
-// app.use(express.json());
-// app.use(express.urlencoded({
-//   extended:false
-// }));
-// dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:false
+}));
+dotenv.config();
 // app.use(cors());
 const PORT = process.env.PORT || 5500;
 const connectDB = require("./database");
-// app.set("./views", path.join(__dirname, "/views"));
-// app.set("view engine");
+app.set("./views", path.join(__dirname, "/views"));
+app.set("view engine");
 app.use("", require("./routes/files"));
 // app.use("/file/download", require("./routes/downloadlink"));
 connectDB();
